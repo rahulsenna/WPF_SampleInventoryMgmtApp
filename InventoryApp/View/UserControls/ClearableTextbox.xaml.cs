11346befd44689916b8c7d24feb6278d039c06e6
Rahul Senna <rahulvsenna@gmail.com>
2026-01-06 14:00:00 +0530

    Clearable text and search

---
+++ InventoryApp/View/UserControls/ClearableTextbox.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace InventoryApp.View.UserControls
+{
+	public partial class ClearableTextbox : UserControl, INotifyPropertyChanged
+	{
+		public ClearableTextbox()
+		{
+			InitializeComponent();
+		}
+
+		private void btnClear_Click(object sender, RoutedEventArgs e)
+		{
+			txtInput.Clear();
+			txtInput.Focus();
+		}
+		public string Text
+		{
+			get { return (string)GetValue(TextProperty); }
+			set { SetValue(TextProperty, value); }
+		}
+		private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			Text = txtInput.Text;
+			UpdatePlaceholderVisibility();
+		}
+
+		public static readonly DependencyProperty TextProperty =
+						DependencyProperty.Register(
+								nameof(Text),
+								typeof(string),
+								typeof(ClearableTextbox),
+								new FrameworkPropertyMetadata(
+										string.Empty,
+										FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+										OnTextChanged));
+		private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			var control = (ClearableTextbox)d;
+			control.txtInput.Text = (string)e.NewValue;
+			control.UpdatePlaceholderVisibility();
+		}
+
+		private void UpdatePlaceholderVisibility()
+		{
+			txtPlaceholder.Visibility = string.IsNullOrEmpty(Text) ? Visibility.Visible : Visibility.Hidden;
+		}
+
+		private string placeholder;
+		public string Placeholder
+		{
+			get { return placeholder; }
+			set
+			{
+				placeholder = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+			txtPlaceholder.Text = Placeholder;
+		}
+	}
+}
--- InventoryApp/ViewModel/MainWindowViewModel.cs
+++ InventoryApp/ViewModel/MainWindowViewModel.cs
@@ -2,6 +2,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
+using System.Windows.Data;
 using InventoryApp.Model;
 using InventoryApp.MVVM;
 using InventoryApp.View;
@@ -10,6 +11,36 @@ namespace InventoryApp.ViewModel
 {
 	public class MainWindowViewModel : INotifyPropertyChanged
 	{
+		private readonly CollectionViewSource _productsViewSource;
+		public MainWindowViewModel()
+		{
+			Categories = [
+				new() { Name = "Category 1" },
+				new() { Name = "Category 2" },
+				new() { Name = "Category 3" },
+				];
+
+			Products = [
+				new Product { Name = "Product1", Price = 3.3m, Quantity = 43, Category=Categories[0] },
+				new Product { Name = "Product2", Price = 2.6m, Quantity = 60, Category=Categories[1] },
+				new Product { Name = "Product3", Price = 500.2m, Quantity = 2, Category=Categories[2] }
+				];
+
+			_productsViewSource = new CollectionViewSource { Source = Products };
+			_productsViewSource.Filter += _productsViewSource_Filter;
+		}
+
+		private void _productsViewSource_Filter(object sender, FilterEventArgs e)
+		{
+			if (e.Item is Product product)
+			{
+				bool matchesSearch = string.IsNullOrWhiteSpace(SearchText) || product.Name.Contains(SearchText, StringComparison.OrdinalIgnoreCase);
+				e.Accepted = matchesSearch;
+			}
+		}
+
+		public ICollectionView ProductsView => _productsViewSource.View;
+
 		private Product slectedProduct;
 		public Product SelectedProduct
 		{
@@ -21,6 +52,23 @@ namespace InventoryApp.ViewModel
 			}
 		}
 		public ObservableCollection<Product> Products { get; set; }
+
+		private string searchText;
+
+		public string SearchText
+		{
+			get { return searchText; }
+			set
+			{
+				searchText = value; OnPropertyChange();
+				_productsViewSource.View.Refresh();
+			}
+		}
+
+		public List<Product> GetAllProducts
+		{
+			get { return Products.Where(p => string.IsNullOrEmpty(SearchText) || p.Name.Contains(SearchText)).ToList(); }
+		}
 		public List<Category> Categories { get; set; }
 
 		public RelayCommand EditCommand => new(execute => EditAction(), canExecute => SelectedProduct != null);
@@ -48,22 +96,6 @@ namespace InventoryApp.ViewModel
 			ProductModal modal = new() { Title = "Edit", DataContext = pvm };
 			modal.ShowDialog();
 		}
-
-		public MainWindowViewModel()
-		{
-			Categories = [
-				new() { Name = "Category 1" },
-				new() { Name = "Category 2" },
-				new() { Name = "Category 3" },
-				];
-
-			Products = [
-				new Product { Name = "Product1", Price = 3.3m, Quantity = 43, Category=Categories[0] },
-				new Product { Name = "Product2", Price = 2.6m, Quantity = 60, Category=Categories[1] },
-				new Product { Name = "Product3", Price = 500.2m, Quantity = 2, Category=Categories[2] }
-				];
-		}
-
 
 		public event PropertyChangedEventHandler? PropertyChanged;
 
